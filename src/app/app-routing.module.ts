e64ce68cf4ca7ec1245fd0dc3a5be6fbc14501bb
Aldf76 <contasfelipe623@gmail.com>
2025-08-05 13:17:17 -0300

    feat : criação de rotas e filhas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin.component/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent,
+    children: [
+      {path:'config', /* Aqui ficará o component já criado configuração*/ }
+    ]
+   }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
