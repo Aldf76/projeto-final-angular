cd83d8012bd1e564db87409eee906c77472ffc0d
Aldf76 <contasfelipe623@gmail.com>
2025-08-06 15:01:54 -0300

    chore: ajustando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin.component/admin.component';
 import { ConfigComponent } from './components/config/config.component';
 import { ListaFuncionariosComponent } from './components/lista-funcionarios/lista-funcionarios.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CadastroFuncionario } from './components/cadastro-funcionario/cadastro-funcionario';
 
 const routes: Routes = [
   {path: 'listaF', component: ListaFuncionariosComponent},
+  {path: 'cadastroF', component:CadastroFuncionario},
+  {path: 'profile', component: ProfileComponent},
   {path: 'admin', component: AdminComponent,
     children: [
       {path:'config', component: ConfigComponent}
